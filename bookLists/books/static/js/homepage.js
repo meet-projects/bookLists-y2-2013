2285fce8688ff563739ba6db2b2664db066d93df
Omri Doron <omri12@cs.huji.ac.il>
2013-07-01 22:06:43 +0300

    we designed more

--- bookLists/books/static/js/homepage.js
+++ bookLists/books/static/js/homepage.js
@@ -1,7 +1,10 @@
+$(document).ready(function() {
+// Handler for .ready() called.
+
 $('#login-over').handlerIn(function () {
 	//$("#login").show()});
 	alert("hello!");
-
+});
 $('#login-over').handlerOut(function () {
 	$("#login").hide()});
-
+});
