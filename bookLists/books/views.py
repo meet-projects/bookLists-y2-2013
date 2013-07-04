b4dee88166f09da042a21a17b62dbeadc2a1b096
Moran Zaks <moran12@cs.huji.ac.il>
2013-07-04 10:22:17 +0300

    user registration, there can only be one base.html

--- bookLists/books/views.py
+++ bookLists/books/views.py
@@ -5,9 +5,41 @@ from models import Book, Genre
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login, logout
+from django import forms
+from django.contrib.auth.models import User
 
+class UserRegistrationForm(forms.Form):
+         first_name = forms.CharField(label=u'first_name')
+         last_name = forms.CharField(label=u'last_name')
+         email = forms.CharField(label=u'Email')
+         password = forms.CharField(label=u'password',widget=forms.PasswordInput)
+         password_again = forms.CharField(label=u'password_again',widget=forms.PasswordInput)
+         
+def sign_up(request):
+        return render(request, 'books/signup.html', {'form': UserRegistrationForm()})
+def register(request):
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+        #TODO Add functionality to save
+                email = form.cleaned_data['email']
+                first_name = form.cleaned_data['first_name']
+                last_name = form.cleaned_data['last_name']
+                password = form.cleaned_data['password']
+                password_again = form.cleaned_data['password_again']
+                if password != password_again:
+                        return render(request, 'books/signup.html', {'form': UserRegistrationForm(), 'message': 'passwords did not match' })
+                elif len(User.objects.filter(username = email)):
+                        return render(request, 'books/signup.html', {'form': UserRegistrationForm(), 'message': 'ERROR: email already exists' })
+                elif "" in [email, first_name, last_name, password, password_again]:
+                        return render(request, 'books/signup.html', {'form': UserRegistrationForm(), 'message': 'all fields should be filled' })
+                else:
+                        user = User.objects.create_user(username = email, email=None, password=password, last_name=last_name, first_name=first_name)
+                        user.save()
+                        
+        
 
-
+        
+        return HttpResponseRedirect('/')
 
 def home(request):
         return render(request, "books/homepage.html", {'user':request.user})
@@ -25,6 +57,8 @@ def get_book(request, book):
         context = {'book':fitBook}
         
 	return render(request, "books/bookpage.html", context)
+
+
 
 def logout_user(request):
 
