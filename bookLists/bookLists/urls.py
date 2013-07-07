a8915b93b439e3ac8f5749b940936999b4712fa8
Costas Mustaklem <costas12@cs.huji.ac.il>
2013-07-07 11:00:27 +0300

    rating a bit

--- bookLists/bookLists/urls.py
+++ bookLists/bookLists/urls.py
@@ -18,7 +18,8 @@ urlpatterns = patterns('',
         url(r'^signup$', views.sign_up),
         url(r'^submitlogout$', views.submitlogout),
         url(r'^profile$', views.get_profile),
-        url(r'^uploadImage$', views.uploadPage), 
+        url(r'^uploadImage$', views.uploadPage),
+        url(r'^(?P<bookName>[\w \']+)/submitrating$', views.submitRating),
                        
     # url(r'^bookLists/', include('bookLists.foo.urls')),
 
